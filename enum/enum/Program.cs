fd01ebae269650e5bf177c147e1523e8b58bac4b
chenstarsQ <1140330611@qq.com>
2018-09-05 22:36:45 +0800

    none

---
+++ enum/enum/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @enum
+{
+    class Program
+    {
+        enum Day { Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday };
+        enum Month : byte { Jan, Feb, Mar, Apr, May, Jun, Jul, Aug, Sep, Oct, Nov, Dec };
+        static void Main(string[] args)
+        {
+            Day today = Day.Monday;
+            int dayNumber = (int)today;
+            Console.WriteLine("{0} is day number #{1}.", today, dayNumber);
+
+            Month thisMonth = Month.Dec;
+            byte monthNumber = (byte)thisMonth;
+            Console.WriteLine("{0} is month number #{1}.", thisMonth, monthNumber);
+            Console.ReadKey();
+            // Output:
+            // Monday is day number #1.
+            // Dec is month number #11.
+        }
+    }
+}
---
+++ test01/test01/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            a = mymain1();
+            Console.WriteLine("a 的值是{0}",a);
+            mymain();
+            mymain1();
+            fun();
+            fun1();
+            Console.ReadKey();
+            
+
+        }
+
+        static int mymain()
+        {
+            int a, b, c;
+            a = 1;
+            b = 2;
+            c = a + b;
+            Console.WriteLine("c 的值是{0}",c);
+            return 0;
+            
+        }
+        static int mymain1()
+        {
+            int a = 3;
+            return a;
+        }
+        static void fun()
+        {
+            int a = 100;
+            int b = 200;
+            int c, d;
+            c = a == b ? 10 : 20;
+            d = a > b ? 30 : 40;
+            Console.WriteLine("c的值是{0}\nd的值是{1}", c, d);
+        }
+        static void fun1()
+        {
+            int a, c;
+            a = 1;
+            //b = ++a;
+            c = a++;
+            //Console.WriteLine("b的值是{0},c的值是{1}", b,c);
+            Console.WriteLine("c的值是{0}", c);
+        }
+
+    }
+}
